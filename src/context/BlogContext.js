5a1d3fd53225197256a2a319f7010f146ba07840
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-11-01 12:15:07 +0100

    app finalization

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,22 +4,13 @@ import jsonServer from "../api/jsonServer";
 const blogReducer = (state, action) => {
     switch (action.type) {
         case "get_blogposts":
-            return action.payload
+            return action.payload;
         case "edit_blogpost":
             return state.map((blogPost) => {
                 return blogPost.id === action.payload.id
                     ? action.payload
                     : blogPost;
             });
-        case "add_blogpost":
-            return [
-                ...state,
-                {
-                    id: Math.floor(Math.random() * 99999),
-                    title: action.payload.title,
-                    content: action.payload.content,
-                },
-            ];
         case "delete_blogpost":
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case "default":
@@ -34,24 +25,31 @@ const getBlogPosts = (dispatch) => {
     };
 };
 
-const addBlogPost = (dispatch) => {
-    return (title, content, callback) => {
-        dispatch({
-            type: "add_blogpost",
-            payload: { title, content },
+const addBlogPost = () => {
+    return async (title, content, callback) => {
+        await jsonServer.post("/blogposts", {
+            title,
+            content,
         });
+
         callback ? callback() : null;
     };
 };
 
 const deleteBlogPost = (dispatch) => {
-    return (id) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`);
         dispatch({ type: "delete_blogpost", payload: id });
     };
 };
 
 const editBlogPost = (dispatch) => {
-    return (id, title, content, callback) => {
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, {
+            title,
+            content,
+        });
+
         dispatch({
             type: "edit_blogpost",
             payload: {
